3e700e52fe0ffa7a73b849ce23e56fad4078e350
gamerthatissus <123601491+gamerthatissus@users.noreply.github.com>
2026-02-11 12:22:50 -0800

    small fix
    e

--- rock protoytype/Assets/fake stone script.cs
+++ rock protoytype/Assets/fake stone script.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class fakestonescript : MonoBehaviour
 {
     public SpriteRenderer HIDEN_until_in_area;
     public SpriteRenderer HIDEN_until_in_area2;
     public SpriteRenderer HIDEN_until_in_area3;
+    public Light2D HIDEN_until_in_area4;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,7 @@ public class fakestonescript : MonoBehaviour
         HIDEN_until_in_area.enabled = false;
         HIDEN_until_in_area2.enabled = false;
         HIDEN_until_in_area3.enabled = false;
+        HIDEN_until_in_area4.enabled = false;
 
 
     }
@@ -25,6 +28,7 @@ public class fakestonescript : MonoBehaviour
             HIDEN_until_in_area.enabled = true;
             HIDEN_until_in_area2.enabled = true;
             HIDEN_until_in_area3.enabled = true;
+            HIDEN_until_in_area4.enabled = true;
 
 
 
