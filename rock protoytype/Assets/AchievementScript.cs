a5563656d693b8f70d077d6197d76c849a1bbd7a
Player386 <maximumparkour11@gmail.com>
2026-03-05 13:18:55 -0800

    achievements progress

--- rock protoytype/Assets/AchievementScript.cs
+++ rock protoytype/Assets/AchievementScript.cs
@@ -1,11 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class AchievementScript : MonoBehaviour
 {
     private string lastSceneName;
+    private bool hitless;
+    private move22 moveScript;
+
+    private void GiveAchievement(string achievement)
+    {
+        string currentAchievements = PlayerPrefs.GetString("achievements");
+
+        if (!currentAchievements.Split(";").Contains(achievement))
+        {
+            PlayerPrefs.SetString("achievements", currentAchievements + achievement + ";");
+            PlayerPrefs.Save();
+
+            Debug.Log(achievement);
+        }
+    }
 
     private void Start()
     {
@@ -22,32 +39,65 @@ public class AchievementScript : MonoBehaviour
     {
         string thisSceneName = SceneManager.GetActiveScene().name;
 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlayerPrefs.DeleteKey("achievements");
+            PlayerPrefs.Save();
+
+            Debug.Log("achievements cleared");
+        }
+
+        if (hitless && moveScript && moveScript.hp < 120)
+        {
+            hitless = false;
+        }
+
         if (thisSceneName != lastSceneName)
         {
             if (lastSceneName == "game")
             {
                 if (thisSceneName == "LevelOne")
                 {
-                    Debug.Log("Completed the tutorial!");
+                    GiveAchievement("Tutorial completed!");
                 }
                 else if (thisSceneName == "THE BOSS FIGHT")
                 {
-                    Debug.Log("Found the secret portal!");
+                    GiveAchievement("Secret portal found!");
                 }
             }
             else if (lastSceneName == "LevelOne" && thisSceneName == "LevelTwo")
             {
-                Debug.Log("Completed level 1!");
+                GiveAchievement("Level 1 completed!");
+
+                if (hitless)
+                {
+                    GiveAchievement("Level 1 completed without taking damage!");
+                }
             }
             else if (lastSceneName == "LevelTwo" && thisSceneName == "LevelThree")
             {
-                Debug.Log("Completed level 2!");
+                GiveAchievement("Level 2 completed!");
+
+                if (hitless)
+                {
+                    GiveAchievement("Level 2 completed without taking damage!");
+                }
             }
             else if (lastSceneName == "LevelThree" && thisSceneName == "THE BOSS FIGHT")
             {
-                Debug.Log("Completed level 3!");
+                GiveAchievement("Level 3 completed!");
+
+                if (hitless)
+                {
+                    GiveAchievement("Level 3 completed without taking damage!");
+                }
             }
 
+            GameObject move = GameObject.Find("move");
+            if (move)
+                moveScript = move.GetComponent<move22>();
+
+            hitless = true;
             lastSceneName = thisSceneName;
         }
     }
