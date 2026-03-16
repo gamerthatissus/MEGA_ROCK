9667d402de67e9e50d2134c71c5c947347e066ab
Player386 <maximumparkour11@gmail.com>
2026-03-16 13:21:20 -0700

    controller works on titlescreen

--- rock protoytype/Assets/Scripts/TitleScreenButtons.cs
+++ rock protoytype/Assets/Scripts/TitleScreenButtons.cs
@@ -1,9 +1,53 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.InputSystem;
+using System;
+using Unity.VisualScripting;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
 
 public class TitleScreenButtons : MonoBehaviour
 {
+    public GameObject startButton;
+    public GameObject quitButton;
+
+    private Action selectedButton;
+
+    private void Start()
+    {
+        selectedButton = TitleStartButton;
+        ExecuteEvents.Execute(quitButton, new PointerEventData(EventSystem.current), ExecuteEvents.pointerExitHandler);
+        ExecuteEvents.Execute(startButton, new PointerEventData(EventSystem.current), ExecuteEvents.pointerEnterHandler);
+    }
+
+    public void MenuScroll(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (selectedButton == TitleStartButton)
+            {
+                selectedButton = TitleQuitButton;
+                ExecuteEvents.Execute(startButton, new PointerEventData(EventSystem.current), ExecuteEvents.pointerExitHandler);
+                ExecuteEvents.Execute(quitButton, new PointerEventData(EventSystem.current), ExecuteEvents.pointerEnterHandler);
+            }
+            else
+            {
+                selectedButton = TitleStartButton;
+                ExecuteEvents.Execute(quitButton, new PointerEventData(EventSystem.current), ExecuteEvents.pointerExitHandler);
+                ExecuteEvents.Execute(startButton, new PointerEventData(EventSystem.current), ExecuteEvents.pointerEnterHandler);
+            }
+        }
+    }
+
+    public void PressButton(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            selectedButton.Invoke();
+        }
+    }
+
     public void TitleStartButton()
     {
         SceneManager.LoadScene("MainMenu");
